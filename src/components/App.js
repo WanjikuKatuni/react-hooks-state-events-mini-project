0bc9b1fc6f44260bd468bb572dced69d50daca85
wanjiku <annritawanjiku77@gmail.com>
2022-10-03 22:34:32 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,38 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+
+  const [tasks, setTasks] = useState(TASKS)
+  const [category, setCategory] = useState("All")
+
+  function handleAddingTask(newTask){
+    setTasks([...tasks, newTask]);
+  }
+
+  function handleDeletingTask(deletedTask){
+    setTasks(tasks.filter((task)=> task.text !== deletedTask))
+  }
+
+  const seenTasks = tasks.filter((task)=> category === "All" || task.category === category);
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter 
+      categories = {CATEGORIES}
+      selectedCategory={category}
+      onSelectedCategory = {setCategory}
+      />
+      <div className="tasks">
+        <h5>Tasksk</h5>
+      <NewTaskForm 
+      categories = {CATEGORIES.filter((cat)=> cat !== "All")}
+      onTaskFormSubmit = {handleAddingTask}
+      />
+      <TaskList 
+      onDeleteTask={handleDeletingTask}
+      tasks={seenTasks}
+      />
+      </div>
     </div>
   );
 }
